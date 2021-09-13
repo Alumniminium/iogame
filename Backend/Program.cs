107b6388b333a467408b8e6ebab727cfa3b433b0
David Fowler <davidfowl@gmail.com>
2021-09-13 09:11:14 -0700

    Update Program.cs
    - Removed SignalR (I don't see where it's being used)
    - Use a route handler for websockets
    - Remove UseRouting
    - Use WebApplication.Create, there's no builder required for this scenario

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,4 +1,3 @@
-using System.Net;
 using System.Net.WebSockets;
 using System.Text;
 using iogame.Net;
@@ -8,51 +7,38 @@ using iogame.Simulation.Entities;
 
 var game = new Game();
 game.Start();
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddSignalR();
-
-var app = builder.Build();
+var app = WebApplication.Create(args);
 
 // Configure the HTTP request pipeline.
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-app.UseRouting();
+
 app.UseWebSockets();
 
-app.Use(async (context, next) =>
+app.MapGet("/chat", async (context) =>
 {
-    if (context.Request.Path == "/chat")
+    if (!context.WebSockets.IsWebSocketRequest)
     {
-        if (context.WebSockets.IsWebSocketRequest)
-        {
-            using WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-            var player = new Player(webSocket); 
-            game.AddPlayer(player);
-
-            var buffer = new byte[1024 * 4];
-            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            while (!result.CloseStatus.HasValue)
-            {
-                PacketHandler.Handle(player,buffer);
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            }
-            game.RemovePlayer(player);
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-        }
-        else
-        {
-            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-        }
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        return;
     }
-    else
+    
+    using WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+    var player = new Player(webSocket); 
+    game.AddPlayer(player);
+
+    var buffer = new byte[1024 * 4];
+    WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+    while (!result.CloseStatus.HasValue)
     {
-        await next();
+        PacketHandler.Handle(player,buffer);
+        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
     }
-
+    game.RemovePlayer(player);
+    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
 });
 
 app.Run();
