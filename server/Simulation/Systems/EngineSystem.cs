3e0f9a0f26e0a49fff3a846e9894b5c8fe44ccf2
trbl <domi@outlook.at>
2022-01-08 18:48:21 +0100

    RCS

--- server/Simulation/Systems/EngineSystem.cs
+++ server/Simulation/Systems/EngineSystem.cs
@@ -40,28 +40,44 @@ namespace server.Simulation.Systems
 
             var propulsion = phy.Forward * (eng.MaxPropulsion * eng.Throttle);
 
-            // if (eng.RCS)
-            // {
-            //     var powerAvailable = 1 - MathF.Abs(eng.Throttle);
-            //     var dangV = turnDirection-phy.RotationRadians;
+            if (eng.RCS)
+            {
+                var powerAvailable = eng.MaxPropulsion * (1 - MathF.Abs(eng.Throttle)) / 2;
 
-            //     var left = (turnDirection-phy.RotationRadians+(Math.PI*2))%(Math.PI*2)>Math.PI;
+                var powerToCancelAngVel = Math.Abs(phy.AngularVelocity * 10);
+                var powerToCancelDrift = Math.Abs(phy.RotationRadians - Vector2.Normalize(phy.Velocity).ToRadians() * phy.Velocity.Length());
 
-            //     if(left)
-            //         phy.RotationRadians += dangV * deltaTime;
-            //     if(!left)
-            //         phy.RotationRadians += -dangV * deltaTime;
+                phy.AngularVelocity *= 1f - Math.Abs(powerToCancelAngVel - powerAvailable);
+                phy.AngularVelocity *= 1f - Math.Abs(powerToCancelAngVel - powerAvailable);
 
-            //     // FConsole.WriteLine($"Rotation Target: {turnDirection}rad Rot Delta: "+ dangV);
+                phy.Velocity = Vector2.Lerp(phy.Velocity, phy.Forward * eng.MaxPropulsion * eng.Throttle, deltaTime );
 
-            //     if (phy.Velocity != Vector2.Zero)
-            //     {
-            //         var deltaDir = phy.Forward - Vector2.Normalize(phy.Velocity);
-            //         var stabilizationPropulsion = deltaDir * eng.MaxPropulsion * powerAvailable;
-            //         stabilizationPropulsion = stabilizationPropulsion.ClampMagnitude(MathF.Min(stabilizationPropulsion.Length(), phy.Velocity.Length()));
-            //         propulsion += stabilizationPropulsion;
-            //     }
-            // }
+
+                // if(turnDirection == 0)
+                //     turnDirection = phy.RotationRadians;
+
+                // var dang = turnDirection-phy.RotationRadians;
+
+                // if(dang != 0)
+                // {
+                //     var left = (turnDirection-phy.RotationRadians+(Math.PI*2))%(Math.PI*2)>Math.PI;
+
+                //     if(left)
+                //         phy.AngularVelocity += dang * deltaTime;
+                //     if(!left)
+                //         phy.AngularVelocity += -dang * deltaTime;
+                // }
+
+                // // FConsole.WriteLine($"Rotation Target: {turnDirection}rad Rot Delta: "+ dangV);
+
+                // if (phy.Velocity != Vector2.Zero)
+                // {
+                //     var deltaDir = phy.Forward - Vector2.Normalize(phy.Velocity);
+                //     var stabilizationPropulsion = -deltaDir * eng.MaxPropulsion * powerAvailable;
+                //     stabilizationPropulsion = stabilizationPropulsion.ClampMagnitude(MathF.Min(stabilizationPropulsion.Length(), phy.Velocity.Length()));
+                //     propulsion += stabilizationPropulsion;
+                // }
+            }
             phy.Acceleration = propulsion;
             phy.AngularVelocity = turnDirection * 3;
         }
