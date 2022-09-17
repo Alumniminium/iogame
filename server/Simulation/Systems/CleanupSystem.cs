942aebde88edb281dfced5d15978e5586f98e4a0
trbl <domi@outlook.at>
2022-09-17 14:31:38 +0200

    change shape sizes

--- server/Simulation/Systems/CleanupSystem.cs
+++ server/Simulation/Systems/CleanupSystem.cs
@@ -6,9 +6,6 @@ namespace server.Simulation.Systems
     public sealed class CleanupSystem : PixelSystem<CollisionComponent>
     {
         public CleanupSystem() : base("Cleanup System", threads: 1) { }
-        public override void Update(in PixelEntity ntt, ref CollisionComponent col)
-        {
-            ntt.Remove<CollisionComponent>();
-        }
+        public override void Update(in PixelEntity ntt, ref CollisionComponent col) => ntt.Remove<CollisionComponent>();
     }
 }
