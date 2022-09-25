cc1b4a02530996def1d006b04dec46d91a7cf16f
trbl <domi@outlook.at>
2022-09-25 10:33:22 +0200

    start to quit fna

--- FNAClient/Managers/GamepadManager.cs
+++ FNAClient/Managers/GamepadManager.cs
@@ -39,7 +39,7 @@ namespace RG351MP.Managers
             LastState = CurrentState;
             CurrentState = GamePad.GetState(0);
 
-            if (Down(Buttons.Back))
+            if (Down(Buttons.Start))
                 Environment.Exit(0);
 
             if(GameScene.Player==null)
